0c953cb1297e0658f70476bd4244c16c8a1d1ddf
Zhijie Ji <52663992+zhijie-ji@users.noreply.github.com>
2021-04-05 10:04:31 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -11,6 +11,6 @@ def hello_world():
 if __name__ == '__main__':
     app.run(
     host=os.getenv('LISTEN', '0.0.0.0'),
-    port=int(os.getenv('PORT', '8080'))
+    port=int(os.getenv('PORT', '5000'))
     )
 
